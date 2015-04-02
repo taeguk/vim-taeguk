1a7a320fff1eafa1bf208ff665d1e12cfce74e65
taeguk <xornrbboy@gmail.com>
2015-04-02 16:03:55 -0400

    shell update

--- install.sh
+++ install.sh
@@ -2,26 +2,32 @@
 
 echo -e "\n ********** vim-taeguk ********** \n"
 
-if [ -e "~/.vim" ]
+echo "[*] checking ~/.vim existed..."
+if [ -e ~/.vim ]
 then
 	echo "[!] existing ~/.vim founded!!"
-	echo -n "Do you delete original .vim ?? (Y/N) >> "
+	echo -n "Do you delete original .vim ?? (Y/N/B) >> "
 	read sel
-	if [ sel != 'Y' && sel != 'y' ]
+	if [ \( sel = 'Y' -o sel = 'y' \) ] && [ \( sel = 'B' -o sel = 'b' \) ] && [ ! -e ~/.vim_bckp ]
 	then
+		mv ~/.vim ~/.vim_bckp
+	else
 		echo "[*] task calcelled!"
 		exit 1
 	fi
 	echo "[*] rm -rf ~/.vim"
 	rm -rf ~/.vim
 fi
-if [ -e "~/.vimrc" ]
+echo "[*] checking ~/.vimrc existed..."
+if [ -e ~/.vimrc ]
 then
 	echo "[!] existing ~/.vimrc founded!!"
-	echo -n "Do you delete original .vimrc ?? (Y/N) >> "
+	echo -n "Do you delete original .vimrc ?? (Y/N/B) >> "
 	read sel
-	if [ sel != 'Y' && sel != 'y' ]
+	if [ \( sel = 'Y' -o sel = 'y' \) ] && [ \( sel = 'B' -o sel = 'b' \) ] && [ ! -e ~/.vimrc_bckp ]
 	then
+		mv ~/.vimrc ~/.vimrc_bckp
+	else
 		echo "[*] task calcelled!"
 		exit 1
 	fi
