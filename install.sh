9e60c587edf1159a73646619fb26aab9d6098697
taeguk <xornrbboy@gmail.com>
2015-04-02 16:16:42 -0400

    shell update4

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ then
 	echo "[!] existing ~/.vim founded!!"
 	echo -n "Do you delete original .vim ?? (Y/N/B) >> "
 	read sel
-	if [ sel = 'Y' || sel = 'y' ]; then
-		true
-	elif [ \( sel = 'B' -o sel = 'b' \) ] && [ ! -e ~/.vim_bckp ]; then
+	if [ $sel = 'Y' || $sel = 'y' ]; then
+		:
+	elif [ \( $sel = 'B' -o $sel = 'b' \) ] && [ ! -e ~/.vim_bckp ]; then
 		mv ~/.vim ~/.vim_bckp
 	else
 		echo "[*] task calcelled!"
@@ -26,9 +26,9 @@ then
 	echo "[!] existing ~/.vimrc founded!!"
 	echo -n "Do you delete original .vimrc ?? (Y/N/B) >> "
 	read sel
-	if [ sel = 'Y' || sel = 'y' ]; then
-		true
-	elif [ \( sel = 'B' -o sel = 'b' \) ] && [ ! -e ~/.vimrc_bckp ]; then
+	if [ $sel = 'Y' || $sel = 'y' ]; then
+		:
+	elif [ \( $sel = 'B' -o $sel = 'b' \) ] && [ ! -e ~/.vimrc_bckp ]; then
 		mv ~/.vimrc ~/.vimrc_bckp
 	else
 		echo "[*] task calcelled!"
